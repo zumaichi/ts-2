bf2d6731b8449dc7a8f397490fbd01e7d037901c
zuma <dzumaquero@gmail.com>
2025-02-10 16:14:01 +0100

    actualiza

--- typescript-sandbox/src/main.ts
+++ typescript-sandbox/src/main.ts
@@ -1,7 +1,39 @@
 import "./style.css";
 
-function cambioTurnos() {
 
-const siguiente =
+let turno = 0; 
+
+const pintaTurno = () => {
+    const marcador = document.getElementsByClassName("numero-turno");
+
+    if(
+        marcador !==null && marcador !==undefined && marcador instanceof HTMLHeadElement){
+
+            marcador.textContent = turno.toString().padStart(2, '0');
+        }
+     else{
+        console.error("no funciona el mardor de numeros")
+     }
+    
+
+}
+
+  
+
+const eventos = () => {
+    const botonSiguiente = document.getElementsByClassName("siguiente");
+
+    if (
+        botonSiguiente !== null && botonSiguiente !== undefined ){
+            botonSiguiente
+        
+
+
+        }
+            
+   
+    
+    
+
 
 }
